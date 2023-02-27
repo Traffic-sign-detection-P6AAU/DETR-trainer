9011b747a539f611525c57aee31b56fd7359bab4
knielsen-afk <knie20@student.aau.dk>
2023-02-27 15:17:36 +0100

    Added prints for time elapsed + images downloaded

--- fetch.py
+++ fetch.py
@@ -12,36 +12,37 @@ response = requests.get("https://distribution.dataudveksler.app.vd.dk/api/datase
 xml = ET.fromstring(response.text)
 
 cameralist = xml[1][3][0][1]
+start = time.time()
+j = 1
+k = 1
+if (k < 10000):
+    for cctvCameraMetadataRecord in cameralist:
+        for elem in cctvCameraMetadataRecord:
+            if elem.tag == "{http://datex2.eu/schema/2/2_0}cctvStillImageService":
+                    url = elem[2][0].text
+                    print("url: " + url)
 
-j = 0
-for cctvCameraMetadataRecord in cameralist:
-    for elem in cctvCameraMetadataRecord:
-        if elem.tag == "{http://datex2.eu/schema/2/2_0}cctvStillImageService":
-                url = elem[2][0].text
-                print("url: " + url)
+                    # Number of images to download from each camera
+                    num_images = 3
 
-                # Number of images to download from each camera
-                num_images = 2
+                    # Directory to save the images to
+                    directory = "data/images/"
 
-                # Directory to save the images to
-                directory = "data/images/"
+                    # Loop over the number of images to download
+                    for i in range(num_images):
+                        # Fetch the image from the API endpoint
+                        response = requests.get(url)
 
-                # Create the directory if it does not exist
-                import os
-                os.makedirs(directory, exist_ok=True)
+                        # Generate a filename for the image
+                        filename = f"{directory}camera_{j}_image_{k}.jpg"
 
-                # Loop over the number of images to download
-                for i in range(num_images):
-                    # Fetch the image from the API endpoint
-                    response = requests.get(url)
-
-                    # Generate a filename for the image
-                    filename = f"{directory}camera_{j}_image_{i+1}.jpg"
-
-                    # Save the image to the specified directory
-                    with open(filename, "wb") as f:
-                        f.write(response.content)
-
-                    # Wait for 20 seconds before fetching the next image
-                    time.sleep(20)
-                j += 1
+                        # Save the image to the specified directory
+                        with open(filename, "wb") as f:
+                            f.write(response.content)
+                        # Wait for 15 seconds before fetching the next image
+                        end = time.time()
+                        print("Images downloaded: ", k)
+                        print(f'Total time elapsed: {round((end-start)/60, 2)} minutes')
+                        k += 1
+                        time.sleep(15)
+                    j += 1
