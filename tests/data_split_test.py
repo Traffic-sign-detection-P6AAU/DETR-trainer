fd4f1a8624295a94117096433d56bf73e5f0cad8
Jakob Frederik Lykke <JakobJFL@gmail.com>
2023-04-14 14:36:28 +0200

    fixed tests

--- tests/data_split_test.py
+++ tests/data_split_test.py
@@ -1,6 +1,21 @@
 import unittest
 from data_handler.data_split import find_annos_and_imgs, divide_data
 
+ACCEPTED_CATS = [
+    {
+        "id": 1,
+        "oldid": 1
+    },
+    {
+        "id": 2,
+        "oldid": 2
+    },
+    {
+        "id": 3,
+        "oldid": 3
+    }
+]
+
 class FindAnnosAndImgsTests(unittest.TestCase):
     def test_imgs_id_off(self):
         labels = {
@@ -8,6 +23,7 @@ class FindAnnosAndImgsTests(unittest.TestCase):
                 {'image_id': 1, 'category_id': 1},
                 {'image_id': 2, 'category_id': 2}
             ],
+            'categories': ACCEPTED_CATS,
             'images': [
                 {'id': 1},
                 {'id': 3}
@@ -18,11 +34,12 @@ class FindAnnosAndImgsTests(unittest.TestCase):
            'annotations': [
                 {'image_id': 1, 'category_id': 1}
            ],
+           'categories': ACCEPTED_CATS,
             'images': [
                 {'id': 1}
             ]
         }
-        result = find_annos_and_imgs(labels, cat_ids)
+        result = find_annos_and_imgs(labels, cat_ids, ACCEPTED_CATS)
         self.assertEqual(result, expected)
 
     def test_image_id_off(self):
@@ -31,6 +48,7 @@ class FindAnnosAndImgsTests(unittest.TestCase):
                 {'image_id': 1, 'category_id': 1},
                 {'image_id': 3, 'category_id': 2}
             ],
+            'categories': ACCEPTED_CATS,
             'images': [
                 {'id': 1},
                 {'id': 2}
@@ -41,11 +59,12 @@ class FindAnnosAndImgsTests(unittest.TestCase):
            'annotations': [
                 {'image_id': 1, 'category_id': 1}
            ],
+           'categories': ACCEPTED_CATS,
             'images': [
                 {'id': 1}
             ]
         }
-        result = find_annos_and_imgs(labels, cat_ids)
+        result = find_annos_and_imgs(labels, cat_ids, ACCEPTED_CATS)
         self.assertEqual(result, expected)
 
     def test_image_id_mis(self):
@@ -53,6 +72,7 @@ class FindAnnosAndImgsTests(unittest.TestCase):
             'annotations': [
                 {'image_id': 2, 'category_id': 2}
             ],
+            'categories': ACCEPTED_CATS,
             'images': [
                 {'id': 1},
                 {'id': 2}
@@ -63,11 +83,12 @@ class FindAnnosAndImgsTests(unittest.TestCase):
            'annotations': [
                 {'image_id': 2, 'category_id': 2}
            ],
+           'categories': ACCEPTED_CATS,
             'images': [
                 {'id': 2}
             ]
         }
-        result = find_annos_and_imgs(labels, cat_ids)
+        result = find_annos_and_imgs(labels, cat_ids, ACCEPTED_CATS)
         self.assertEqual(result, expected)
         
     def test_cat_id_off(self):
@@ -76,6 +97,7 @@ class FindAnnosAndImgsTests(unittest.TestCase):
                 {'image_id': 1, 'category_id': 1},
                 {'image_id': 2, 'category_id': 3}
             ],
+            'categories': ACCEPTED_CATS,
             'images': [
                 {'id': 1},
                 {'id': 2}
@@ -86,11 +108,12 @@ class FindAnnosAndImgsTests(unittest.TestCase):
            'annotations': [
                 {'image_id': 1, 'category_id': 1}
            ],
+            'categories': ACCEPTED_CATS,
             'images': [
                 {'id': 1}
             ]
         }
-        result = find_annos_and_imgs(labels, cat_ids)
+        result = find_annos_and_imgs(labels, cat_ids, ACCEPTED_CATS)
         self.assertEqual(result, expected)
 
     def test_all_good(self):
@@ -99,13 +122,14 @@ class FindAnnosAndImgsTests(unittest.TestCase):
                 {'image_id': 1, 'category_id': 1},
                 {'image_id': 5, 'category_id': 3}
             ],
+            'categories': ACCEPTED_CATS,
             'images': [
                 {'id': 1},
                 {'id': 5}
             ]
         }
         cat_ids = [1, 3]
-        result = find_annos_and_imgs(labels, cat_ids)
+        result = find_annos_and_imgs(labels, cat_ids, ACCEPTED_CATS)
         self.assertEqual(result, labels)
 
 class DivideDataTest(unittest.TestCase):
@@ -126,16 +150,19 @@ class DivideDataTest(unittest.TestCase):
                 'annotations': [
                     {'image_id': 1, 'category_id': 1}
                 ],
+                'categories': ACCEPTED_CATS,
                 'images': [{'id': 1}],
             },
             {
                 'annotations': [
                     {'image_id': 5, 'category_id': 3}
                 ],
+                'categories': ACCEPTED_CATS,
                 'images': [{'id': 5}],
             }
         )
-        res = divide_data(data, cat_ids)
+        res = divide_data(data, cat_ids, ACCEPTED_CATS)
+        print(res)
         self.assertEqual(res, expected)
     def test_uneven_imgs(self):
         data = {
@@ -156,6 +183,7 @@ class DivideDataTest(unittest.TestCase):
                 'annotations': [
                     {'image_id': 1, 'category_id': 1}
                 ],
+                'categories': ACCEPTED_CATS,
                 'images': [{'id': 1}],
             },
             {
@@ -163,10 +191,11 @@ class DivideDataTest(unittest.TestCase):
                     {'image_id': 2, 'category_id': 3},
                     {'image_id': 5, 'category_id': 3}
                 ],
+                'categories': ACCEPTED_CATS,
                 'images': [{'id': 2}, {'id': 5}],
             }
         )
-        res = divide_data(data, cat_ids)
+        res = divide_data(data, cat_ids, ACCEPTED_CATS)
         self.assertEqual(res, expected)
 
 if __name__ == '__main__':
