5c414175a87ff8b16d1f4a00a49fc5b52e83ee74
knielsen-afk <knie20@student.aau.dk>
2023-04-17 10:44:03 +0200

    started on evaluation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from trainer.img_show import show_img_w_prediction, show_model_prediction
 from trainer.data_loader import load_datasets, get_dataloaders, get_id2label
 from trainer.train import start_training
 from trainer.model import save_model, get_model, get_img_processor
-from trainer.settings import CHECKPOINT, MODEL_PATH
+from trainer.settings import MODEL_PATH
 from data_handler.data_split import split_dataset
 from data_handler.data_labeler import extend_annotations
 
@@ -17,12 +17,10 @@ def main():
     choice = input()
     if choice == '1':
         image_processor = get_img_processor()
-        pre_model = get_model(CHECKPOINT)
-        # show_img_w_prediction(image_processor, pre_model)
         train_dataset, val_dataset, test_dataset = load_datasets(image_processor)
         train_dataloader, val_dataloader, test_dataloader = get_dataloaders(image_processor, train_dataset, val_dataset, test_dataset)
         trained_model = start_training(train_dataloader, val_dataloader, get_id2label(train_dataset))
-        #show_model_prediction(test_dataset, image_processor, trained_model)
+        evaluate()
         save_model(trained_model)
     elif choice == '2':
         image_processor = get_img_processor()
