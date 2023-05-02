84ae25c58696252271d06c936e8dbbba2a787557
Jakob Frederik Lykke <JakobJFL@gmail.com>
2023-05-02 16:06:55 +0200

    fixed img_show

--- data_handler/img_show.py
+++ data_handler/img_show.py
@@ -5,19 +5,20 @@ import cv2
 import supervision as sv
 from settings import CONFIDENCE_TRESHOLD, IOU_TRESHOLD
 from data_handler.shared import load_json
-
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+from data_handler.data_loader import get_id2label
 
 def show_img_w_prediction(image_processor, model, categories_path): # disable gradient calculation reduse memory 
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
     with torch.no_grad():
 
         # load image and predict
         image = cv2.imread('../Datasets/google_maps/test3.jpg')
-        inputs = image_processor(images=image, return_tensors='pt').to(DEVICE)
+        inputs = image_processor(images=image, return_tensors='pt').to(device)
         outputs = model(**inputs)
 
         # post-process
-        target_sizes = torch.tensor([image.shape[:2]]).to(DEVICE)
+        target_sizes = torch.tensor([image.shape[:2]]).to(device)
         results = image_processor.post_process_object_detection(
             outputs=outputs,
             threshold=CONFIDENCE_TRESHOLD,
@@ -38,6 +39,7 @@ def show_img_w_prediction(image_processor, model, categories_path): # disable gr
     sv.show_frame_in_notebook(frame, (16, 16))
 
 def show_model_prediction(test_dataset, image_processor, model):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     # utils
     categories = test_dataset.coco.cats
     id2label = {k: v['name'] for k,v in categories.items()}
@@ -67,11 +69,11 @@ def show_model_prediction(test_dataset, image_processor, model):
     with torch.no_grad():
 
         # load image and predict
-        inputs = image_processor(images=image, return_tensors='pt').to(DEVICE)
+        inputs = image_processor(images=image, return_tensors='pt').to(device)
         outputs = model(**inputs)
 
         # post-process
-        target_sizes = torch.tensor([image.shape[:2]]).to(DEVICE)
+        target_sizes = torch.tensor([image.shape[:2]]).to(device)
         results = image_processor.post_process_object_detection(
             outputs=outputs, 
             threshold=CONFIDENCE_TRESHOLD, 
